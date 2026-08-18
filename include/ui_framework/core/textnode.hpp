19ddf212e4cecfe18e818ad42df6eeeb73533b21
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-19 02:37:47 +0300

    Add framework-owned TextNode interface

---
+++ include/ui_framework/core/textnode.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "node.hpp"
+
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <string>
+
+namespace ui
+{
+
+    class TextNode : public Node
+    {
+    public:
+        TextNode() = default;
+        ~TextNode() override;
+
+        const std::string &getText() const noexcept;
+        void setText(std::string text);
+
+        TTF_Font *getFont() const noexcept;
+        void setFont(TTF_Font *font);
+
+        TextAlignment getHorizontalAlignment() const noexcept;
+        void setHorizontalAlignment(TextAlignment alignment);
+
+        TextAlignment getVerticalAlignment() const noexcept;
+        void setVerticalAlignment(TextAlignment alignment);
+
+        float getWrapWidth() const noexcept;
+        void setWrapWidth(float width);
+
+    protected:
+        LayoutSize measure(MeasureContext &ctx) override;
+        void draw(SDL_Renderer *renderer) override;
+
+    private:
+        void releaseTextObject() noexcept;
+        void ensureTextObject(SDL_Renderer *renderer);
+
+        std::string text_;
+        TTF_Font *font_ = nullptr;
+
+        TextAlignment horizontalAlignment_ = TextAlignment::START;
+        TextAlignment verticalAlignment_ = TextAlignment::START;
+        float wrapWidth_ = 0.0f;
+
+        SDL_Renderer *cachedRenderer_ = nullptr;
+        TTF_TextEngine *textEngine_ = nullptr;
+        TTF_Text *textObject_ = nullptr;
+    };
+
+}
