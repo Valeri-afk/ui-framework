64673d92ca69af42df62b2315e396d1a8f9b367c
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-18 20:09:45 +0300

    Promote Phase 1 PanelNode ownership API

--- include/ui_framework/core/panelnode.hpp
+++ include/ui_framework/core/panelnode.hpp
@@ -15,7 +15,7 @@ namespace ui
         ~PanelNode() override;
 
         Node *add(std::unique_ptr<Node> child, size_t index);
-        std::unique_ptr<Node> remove(Node &child);
+        void remove(Node &child);
 
         size_t childCount() const noexcept;
         bool hasChildren() const noexcept;
