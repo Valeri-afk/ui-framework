e28ee2fc1717690f92cbfb50c0814847367c5247
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2026-08-19 02:24:06 +0300

    Add internal layout constraint interface

---
+++ include/ui_framework/core/layout_constraints.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ui_framework/core/node.hpp"
+#include "ui_framework/types.hpp"
+
+namespace ui::internal
+{
+
+    LayoutSize resolveMeasurementProposal(
+        const Node &node,
+        LayoutSize proposal) noexcept;
+
+    LayoutSize resolveFinalSize(
+        const Node &node,
+        LayoutSize allocated) noexcept;
+
+}
